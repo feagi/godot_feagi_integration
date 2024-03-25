bb174fc8767c6715240ef738a79775780950698a
Amir Rasteg <amir.rasteg@protonmail.com>
2024-03-25 16:31:58 -0400

    add forgotten threshold

--- addons/FeagiIntegration/Window/FEAGIActionMapping/FEAGIActionMap.gd
+++ addons/FeagiIntegration/Window/FEAGIActionMapping/FEAGIActionMap.gd
@@ -1,16 +1,18 @@
+@tool
 extends RefCounted
 class_name FEAGIActionMap
 ## Essentially just a data structure to pass mapping information
 
-const VAR_NAMES: PackedStringArray = ["OPU_mapping_to", "neuron_X_index", "godot_action", "pass_FEAGI_weight_instead_of_max", "optional_signal_name"]
+const VAR_NAMES: PackedStringArray = ["OPU_mapping_to", "neuron_X_index", "godot_action", "threshold", "pass_FEAGI_weight_instead_of_max", "optional_signal_name"]
 
 var OPU_mapping_to: StringName
 var neuron_X_index: int
 var godot_action: StringName
+var threshold: float = 0.5
 var pass_FEAGI_weight_instead_of_max: bool
 var optional_signal_name: StringName
 
-func _init(OPU_mapping_to_: StringName, neuron_X_index_: int, godot_action_: StringName, pass_FEAGI_weight_instead_of_max_: bool, optional_signal_name_: StringName) -> void:
+func _init(OPU_mapping_to_: StringName, neuron_X_index_: int, godot_action_: StringName, threshold_: float, pass_FEAGI_weight_instead_of_max_: bool, optional_signal_name_: StringName) -> void:
 	OPU_mapping_to = OPU_mapping_to_
 	neuron_X_index = neuron_X_index_
 	godot_action = godot_action_
@@ -31,16 +33,18 @@ static func create_from_valid_dict(dict: Dictionary) -> FEAGIActionMap:
 		dict["OPU_mapping_to"],
 		dict["neuron_X_index"],
 		dict["godot_action"],
+		dict["threshold"],
 		dict["pass_FEAGI_weight_instead_of_max"],
 		dict["optional_signal_name"],
 	)
 
 ## Exports the contents of this object as a json for easy json export
 func export_as_dictionary() -> Dictionary:
-	var dict_out: Dictionary = {}
-	dict_out["OPU_mapping_to"] = str(OPU_mapping_to)
-	dict_out["neuron_X_index"] = neuron_X_index
-	dict_out["godot_action"] = str(godot_action)
-	dict_out["pass_FEAGI_weight_instead_of_max"] = pass_FEAGI_weight_instead_of_max
-	dict_out["optional_signal_name"] = str(optional_signal_name)
-	return dict_out
+	return {
+		"OPU_mapping_to": str(OPU_mapping_to),
+		"neuron_X_index": neuron_X_index,
+		"godot_action": str(godot_action),
+		"threshold": threshold,
+		"pass_FEAGI_weight_instead_of_max": pass_FEAGI_weight_instead_of_max,
+		"optional_signal_name": str(optional_signal_name),
+	}
