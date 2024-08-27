50c0d225dd392550bff11ffcc46cf624ef8c8202
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-08-27 19:20:15 -0400

    genome mapping

---
+++ addons/FeagiIntegration/ResourceScripts/FEAGIGenomeMapping.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name FEAGIGenomeMapping
+
+@export var FEAGI_enabled: bool = true
+@export var delay_seconds_between_frames: float = 0.05
+
+@export var sensor_cameras: Array[FEAGISensoryCamera] = []
