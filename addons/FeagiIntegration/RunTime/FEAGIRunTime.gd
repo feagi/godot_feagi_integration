2721c36f6188d6856e668875d6e1d34eedea9795
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-08-28 11:21:48 -0400

    Start checks

--- addons/FeagiIntegration/RunTime/FEAGIRunTime.gd
+++ addons/FeagiIntegration/RunTime/FEAGIRunTime.gd
@@ -4,9 +4,25 @@ extends Node
 signal sensory_setup_event()
 signal motor_setup_event()
 
+var mapping_config: FEAGIGenomeMapping
+var endpoint_config: FEAGIResourceEndpoint
+
 # Read / verify configs
 # initial loading and setup of variables, check if enabled
 # emit signals when tree is ready, have devices register
 # initialize debugger views
 # confirm network connection to feagi
 # start tick system
+
+func _enter_tree() -> void:
+	mapping_config = load(FEAGIPluginInit.get_genome_mapping_path())
+	endpoint_config = load(FEAGIPluginInit.get_endpoint_path())
+	
+	if !mapping_config:
+		push_error("FEAGI: No settings found for FEAGI configuration! The FEAGI integration will now halt!")
+		return
+	if !endpoint_config:
+		push_error("FEAGI: No connection settings found for FEAGI configuration! THe FEAGI integration will now halt!")
+		return
+	
+	
