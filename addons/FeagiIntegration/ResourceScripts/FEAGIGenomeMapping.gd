1b351b54104adccddd32ef9047b8fa11e5dd057a
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-08-29 12:49:24 -0400

    made sensor registery more generic

--- addons/FeagiIntegration/ResourceScripts/FEAGIGenomeMapping.gd
+++ addons/FeagiIntegration/ResourceScripts/FEAGIGenomeMapping.gd
@@ -5,13 +5,19 @@ class_name FEAGIGenomeMapping
 @export var FEAGI_enabled: bool = true
 @export var delay_seconds_between_frames: float = 0.05
 @export var configuration_JSON: StringName = ""
+@export var allowed_sensor_device_types: PackedStringArray = ["camera"]
 
-#TODO use only a single dictionary?
-@export var sensor_cameras: Dictionary = {} ## A dictionary of [FEAGISensoryCamera]s key'd by their string device name
+@export var sensors: Dictionary = {} ## A dictionary of [FEAGISensoryBase]s key'd by their device type name + "_" + device name
 
-## Allows for registering cameras 
-func sensor_camera_register(device_name: StringName, image_retrival_function: Callable) -> FEAGISensoryCamera:
-	if device_name not in sensor_cameras:
+func sensor_register(device_type_name: StringName, device_name: StringName, data_retrieval_function: Callable) -> FEAGISensoryBase:
+	if device_type_name not in allowed_sensor_device_types:
+		push_error("FEAGI: Unknown sensor of type %s attempted to register itself!" % device_type_name)
 		return null
-	(sensor_cameras[device_name] as FEAGISensoryCamera).register_camera(image_retrival_function)
-	return (sensor_cameras[device_name] as FEAGISensoryCamera)
+	var key: StringName = device_type_name + "_" + device_name
+	if key not in sensors:
+		push_error("FEAGI: Unknown %s sensor of name %s attempted to register itself! Is it in the Genome Mapping?" % [device_type_name, device_name])
+		return null
+	var sensor_definition: FEAGISensoryBase = sensors[key]
+	sensor_definition.register_sensor(data_retrieval_function)
+	return sensor_definition
+	
--- addons/FeagiIntegration/ResourceScripts/IO/Sensory/FEAGISensoryBase.gd
+++ addons/FeagiIntegration/ResourceScripts/IO/Sensory/FEAGISensoryBase.gd
@@ -7,8 +7,22 @@ signal finished_processing_data_for_tick()
 
 var output_data: PackedByteArray
 
+var _data_grabber: Callable = Callable() # Make sure for the proper class, that this callable returns the expected type
+
 func get_data_as_byte_array() -> PackedByteArray:
 	assert(true, "Do not use 'FEAGISensoryBase' Directly!")
 	return PackedByteArray()
 
+# Register a sensor to this object
+func register_sensor(data_grabbing_function: Callable) -> void:
+	if !_data_grabber.is_null():
+		push_warning("FEAGI: A sensor attempted to register itself to %s when it was already registered to another sensor! Overwriting the registration!" % device_name)
+	_data_grabber = data_grabbing_function
+
+## Have a sensor deregister itself
+func deregister_sensor() -> void:
+	if _data_grabber.is_null():
+		push_warning("FEAGI: A call to deregister sensor %s was made when it had nothing registered anyways! Skipping!" % device_name)
+		return
+	_data_grabber = Callable()
 #TODO
