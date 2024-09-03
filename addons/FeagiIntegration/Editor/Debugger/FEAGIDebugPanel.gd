7d6283384c3647788adb9c8b0e36669b93423189
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-09-02 22:02:08 -0400

    debug starting to work

--- addons/FeagiIntegration/Editor/Debugger/FEAGIDebugPanel.gd
+++ addons/FeagiIntegration/Editor/Debugger/FEAGIDebugPanel.gd
@@ -3,14 +3,21 @@ extends MarginContainer
 class_name FEAGIDebugPanel
 ## The actual Debug Panel visible in editor to see what the FEAGI integration is doing
 
+const CAMERA_DEVICE: PackedScene = preload("res://addons/FeagiIntegration/Editor/Debugger/SensoryDevices/FEAGIDebuggerViewCamera.tscn")
+
 var _not_running: VBoxContainer
 var _running: VBoxContainer
+var _sensor_holder: VBoxContainer
+var _motor_holder: VBoxContainer
 
+var _device_update_callables: Array = []
 
 ## THis functiojn is called directly by [FEAGIDebugger] on the init
 func initialize() -> void:
 	_not_running = $Tabs/Data/NotRunning
 	_running = $Tabs/Data/Running
+	_sensor_holder = $Tabs/Data/Running/HSplitContainer/Sensors/PanelContainer/VBoxContainer
+	_motor_holder = $Tabs/Data/Running/HSplitContainer/Motors/PanelContainer/VBoxContainer
 	var data_split: SplitContainer = $Tabs/Data/Running/HSplitContainer
 	data_split.split_offset = size.x / 2
 
@@ -21,4 +28,20 @@ func set_running_state(is_running: bool) -> void:
 
 ## Called by [FEAGIDebugger] when it recieves a message about a device being added
 func add_sensor_device(sensor_type: StringName, sensor_name: StringName) -> void:
-	print("a" + sensor_type + sensor_name)
+	var view: FEAGIDebugViewBase
+	match(sensor_type): # This could be a dict lookup, hmmm
+		"camera":
+			view = CAMERA_DEVICE.instantiate()
+		_:
+			push_error("FEAGI: Unknown device type %s requested to be added to the debugger!" % sensor_type)
+			return
+	view.initialize()
+	view.setup_base(sensor_name)
+	_sensor_holder.add_child(view)
+	_device_update_callables.append(view.update_visualization)
+	_update_none_label(_sensor_holder)
+
+
+func _update_none_label(vbox: VBoxContainer) -> void:
+	var label: Label = vbox.get_child(0)
+	label.visible = vbox.get_child_count() == 1
--- addons/FeagiIntegration/Editor/Debugger/SensoryDevices/FEAGIDebuggerViewCamera.gd
+++ addons/FeagiIntegration/Editor/Debugger/SensoryDevices/FEAGIDebuggerViewCamera.gd
@@ -1,3 +1,4 @@
+@tool
 extends FEAGIDebugViewBase
 class_name FEAGIDebuggerViewCamera
 
