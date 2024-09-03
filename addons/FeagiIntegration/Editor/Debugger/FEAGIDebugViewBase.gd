5f0dd281324a2fe5217e9d3e85f859f9fd1c02f6
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-09-02 21:48:43 -0400

    Visualizer initial work

---
+++ addons/FeagiIntegration/Editor/Debugger/FEAGIDebugViewBase.gd
@@ -0,0 +1,33 @@
+@tool
+extends VBoxContainer
+class_name FEAGIDebugViewBase
+
+var _title: Label
+var _collapse_button: TextureButton
+var _is_collapsed: bool = true
+
+func initialize() -> void:
+	_title = $Title/MarginContainer/VBoxContainer/HBoxContainer/Name
+	_collapse_button = $Title/MarginContainer/VBoxContainer/HBoxContainer/Collapse
+	_collapse_button.pressed.connect(_toggle_collapse)
+
+func setup_base(device_FEAGI_name: StringName) -> void:
+	_title.text = device_FEAGI_name
+
+func set_collapsed(collapse: bool) -> void:
+	if collapse:
+		_collapse_button.texture_normal = load("res://addons/FeagiIntegration/Window/Resources/Icons/Triangle_Right_S.png")
+		_collapse_button.texture_hover = load("res://addons/FeagiIntegration/Window/Resources/Icons/Triangle_Right_H.png")
+		_collapse_button.texture_pressed = load("res://addons/FeagiIntegration/Window/Resources/Icons/Triangle_Right_C.png")
+	else:
+		_collapse_button.texture_normal = load("res://addons/FeagiIntegration/Window/Resources/Icons/Triangle_Down_S.png")
+		_collapse_button.texture_hover = load("res://addons/FeagiIntegration/Window/Resources/Icons/Triangle_Down_H.png")
+		_collapse_button.texture_pressed = load("res://addons/FeagiIntegration/Window/Resources/Icons/Triangle_Down_C.png")
+	_is_collapsed = collapse
+
+# Override this in child classes
+func update_visualization(data: Variant) -> void:
+	return
+
+func _toggle_collapse() -> void:
+	set_collapsed(!_is_collapsed)
---
+++ addons/FeagiIntegration/Editor/Debugger/SensoryDevices/FEAGIDebuggerViewCamera.gd
@@ -0,0 +1,15 @@
+extends FEAGIDebugViewBase
+class_name FEAGIDebuggerViewCamera
+
+var _texture: TextureRect
+
+func initialize() -> void:
+	super()
+	_texture = $Title/MarginContainer/VBoxContainer/MarginContainer/TextureRect
+
+## Takes an [Image]
+func update_visualization(data: Variant) -> void:
+	if !(data is Image): # Funni gdscript formatting issue
+		push_error("FEAGI: Camera Debug View did not recieve an Image! Ignoring!")
+		return
+	(_texture.texture as ImageTexture).set_image(data as Image)
