36dd858e646eeac2cd52e61def056fa0d3b403a3
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-10-28 19:25:34 -0400

    postmessage startup mode

--- addons/FeagiIntegration/RunTime/Autoloads/FEAGI_RunTime.gd
+++ addons/FeagiIntegration/RunTime/Autoloads/FEAGI_RunTime.gd
@@ -39,6 +39,8 @@ func is_ready_for_device_registration() -> bool:
 func initialize_FEAGI_runtime(mapping_config: FEAGI_Genome_Mapping = null, endpoint_config: FEAGI_Resource_Endpoint = null) -> void:
 	print("FEAGI Interface starting up!")
 	
+	var is_in_post_message_mode: bool = FEAGI_JS.attempt_get_parameter_from_URL("postmessage") != null ## In post message mode, we are using an emulated fake feagi runnign in browser
+	
 	# If the mapping config isnt defined, attempt to load it from disk. If still not defined -> failure
 	if !mapping_config:
 		if !FEAGI_PLUGIN_CONFIG.does_mapping_file_exist():
@@ -54,31 +56,36 @@ func initialize_FEAGI_runtime(mapping_config: FEAGI_Genome_Mapping = null, endpo
 		push_warning("FEAGI: FEAGI disabled as per configuration")
 		return
 	
-	# If endpoint isnt defined, attempt to load it from disk. If still not defined -> create one with localhost settings
-	# NOTE: Regardless of what endpoint settings are in the object at this point, they can still be overwritten by URL parameters!
-	if !endpoint_config:
-		if !FEAGI_PLUGIN_CONFIG.does_endpoint_file_exist():
-			push_warning("FEAGI: No endpoint settings were given or found on disk. Defaulting to localhost settings for initial endpoint!")
-			endpoint_config = FEAGI_Resource_Endpoint.new()
-		else:
-			endpoint_config = load(FEAGI_PLUGIN_CONFIG.get_endpoint_path())
+	if !is_in_post_message_mode: # we dont care about this in post message mode
+		# If endpoint isnt defined, attempt to load it from disk. If still not defined -> create one with localhost settings
+		# NOTE: Regardless of what endpoint settings are in the object at this point, they can still be overwritten by URL parameters!
 		if !endpoint_config:
-			push_warning("FEAGI: Endpoint file found but was unable to be read! Defaulting to localhost settings for initial endpoint!")
-			endpoint_config = FEAGI_Resource_Endpoint.new()
+			if !FEAGI_PLUGIN_CONFIG.does_endpoint_file_exist():
+				push_warning("FEAGI: No endpoint settings were given or found on disk. Defaulting to localhost settings for initial endpoint!")
+				endpoint_config = FEAGI_Resource_Endpoint.new()
+			else:
+				endpoint_config = load(FEAGI_PLUGIN_CONFIG.get_endpoint_path())
+			if !endpoint_config:
+				push_warning("FEAGI: Endpoint file found but was unable to be read! Defaulting to localhost settings for initial endpoint!")
+				endpoint_config = FEAGI_Resource_Endpoint.new()
 	
-	# Load in sensor FEAGI Device objects
-	for incoming_FEAGI_sensor in mapping_config.sensors.values():
-		if incoming_FEAGI_sensor is not FEAGI_Device_Sensor_Base:
-			push_error("FEAGI: Unknown object attempted to be added as a FEAGI Sensor! Skipping!")
-			continue
-		if (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).is_disabled:
-			print("FEAGI: Sensor %s is disabled! Not using it for the debugger AND for FEAGI!" % (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name)
-			continue
-		if (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name in _FEAGI_sensors:
-			push_error("FEAGI: Sensor %s is already defined! Ensure there are no sensor devices with repeating names! Skipping" % (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name)
-			continue
-		_FEAGI_sensors[(incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name] = incoming_FEAGI_sensor
+	if is_in_post_message_mode:
+		push_warning("FEAGI: Loaded in PostMessage Mode! This means that we expect to use the browserside FakeFeagi. All Sensors will be disabled!")
+	else:
+		# Load in sensor FEAGI Device objects
+		for incoming_FEAGI_sensor in mapping_config.sensors.values():
+			if incoming_FEAGI_sensor is not FEAGI_Device_Sensor_Base:
+				push_error("FEAGI: Unknown object attempted to be added as a FEAGI Sensor! Skipping!")
+				continue
+			if (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).is_disabled:
+				print("FEAGI: Sensor %s is disabled! Not using it for the debugger AND for FEAGI!" % (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name)
+				continue
+			if (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name in _FEAGI_sensors:
+				push_error("FEAGI: Sensor %s is already defined! Ensure there are no sensor devices with repeating names! Skipping" % (incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name)
+				continue
+			_FEAGI_sensors[(incoming_FEAGI_sensor as FEAGI_Device_Sensor_Base).device_friendly_name] = incoming_FEAGI_sensor
 	
+	#NOTE: We always load motors, whether we are in post message mode or not
 	# Load in motor FEAGI Device objects
 	for outgoing_FEAGI_motor in mapping_config.motors.values():
 		if outgoing_FEAGI_motor is not FEAGI_Device_Motor_Base:
@@ -91,13 +98,17 @@ func initialize_FEAGI_runtime(mapping_config: FEAGI_Genome_Mapping = null, endpo
 			push_error("FEAGI: Motor %s is already defined! Ensure there are no motor devices with repeating names! Skipping" % (outgoing_FEAGI_motor as FEAGI_Device_Motor_Base).device_friendly_name)
 			continue
 		_FEAGI_motors[(outgoing_FEAGI_motor as FEAGI_Device_Motor_Base).device_friendly_name] = outgoing_FEAGI_motor
-	
-	
+
 	# Activate FEAGI Device manager, including the Debugger (if enabled) and the FEAGI Network Interface (if enabled)
 	_FEAGI_device_manager = FEAGI_RunTime_FEAGIDeviceManager.new(_FEAGI_sensors, _FEAGI_motors)
 	if mapping_config.debugger_enabled:
 		_FEAGI_device_manager.setup_debugger()
-	if mapping_config.FEAGI_enabled:
+	if is_in_post_message_mode:
+		var connection_succesful: bool = _FEAGI_device_manager.setup_FEAGI_fake(self)
+		if !connection_succesful:
+			push_error("FEAGI: Unable to initialize PostMessage mode! Integration will now halt")
+			return
+	else:
 		var connection_succesful: bool = await _FEAGI_device_manager.setup_FEAGI_networking(endpoint_config, self)
 		if connection_succesful:
 			_FEAGI_device_manager.send_configurator_and_enable(mapping_config.configuration_JSON)
@@ -117,17 +128,18 @@ func initialize_FEAGI_runtime(mapping_config: FEAGI_Genome_Mapping = null, endpo
 	print("FEAGI: Adding all automatically generated Godot Devices as per configuration!")
 	_automatic_device_generator.add_all_autogenerated_objects(_FEAGI_sensors, _FEAGI_motors)
 	
-	# Initialize tick engine to poll sensors
-	_tick_engine = FEAGI_RunTime_TickEngine.new()
-	add_child(_tick_engine)
-	_tick_engine.setup(mapping_config.delay_seconds_between_frames)
-	_tick_engine.tick.connect(_FEAGI_device_manager.on_sensor_tick)
-	print("FEAGI: Sensor Tick Engine is now enabled!")
-	
-	# Initialize Metric Reporting
-	_metric_reporting = FEAGI_RunTime_MetricReporting.new(endpoint_config.get_full_FEAGI_API_URL(), self)
-	ready_for_metric_posting.emit()
-	print("FEAGI: Metrics are now enabled!")
-	print("FEAGI: FEAGI plugin Initialization is now complete!")
+	if !is_in_post_message_mode:
+		# Initialize tick engine to poll sensors
+		_tick_engine = FEAGI_RunTime_TickEngine.new()
+		add_child(_tick_engine)
+		_tick_engine.setup(mapping_config.delay_seconds_between_frames)
+		_tick_engine.tick.connect(_FEAGI_device_manager.on_sensor_tick)
+		print("FEAGI: Sensor Tick Engine is now enabled!")
+		
+		# Initialize Metric Reporting
+		_metric_reporting = FEAGI_RunTime_MetricReporting.new(endpoint_config.get_full_FEAGI_API_URL(), self)
+		ready_for_metric_posting.emit()
+		print("FEAGI: Metrics are now enabled!")
+		print("FEAGI: FEAGI plugin Initialization is now complete!")
 	
 	
--- addons/FeagiIntegration/RunTime/Autoloads/FEAGI_RunTime_FEAGIDeviceManager.gd
+++ addons/FeagiIntegration/RunTime/Autoloads/FEAGI_RunTime_FEAGIDeviceManager.gd
@@ -39,8 +39,8 @@ func setup_debugger() -> void:
 ## An ASYNC function that initiates the API and websocket conneciton to FEAGI and the connector, but nothing else. Returns true if succesful
 func setup_FEAGI_networking(endpoint: FEAGI_Resource_Endpoint, parent_node: Node) -> bool:
 	_FEAGI_interface = FEAGI_RunTime_FEAGIInterface.new()
-	_FEAGI_interface.socket_closed.connect(_on_socket_close)
-	_FEAGI_interface.name = "FEAGI Networking"
+	_FEAGI_interface.interface_closed.connect(_on_interface_close)
+	_FEAGI_interface.name = "FEAGI Networking WS"
 	parent_node.add_child(_FEAGI_interface)
 	
 	if FEAGI_JS.is_web_build() and endpoint.contains_all_URL_parameters_needed_for_URL_parsing():
@@ -71,19 +71,35 @@ func setup_FEAGI_networking(endpoint: FEAGI_Resource_Endpoint, parent_node: Node
 		_FEAGI_interface = null
 		return false
 	print("FEAGI: Connected to connector websocket at %s!" % endpoint.get_full_connector_ws_URL())
-
 	# setup cache device references
 	_FEAGI_interface.set_cached_device_dicts(_FEAGI_sensors_reference, _FEAGI_motors_reference)
 	
+	# connect motor signals
+	if _debug_interface:
+		_FEAGI_interface.interface_recieved_motor_data.connect(_debug_interface.alert_debugger_about_motor_update)
 	return true
 
+## Sets up a connection to a "Fake FEAGI" running locally in browser. Only useful for demos due to extremely limited functions
+func setup_FEAGI_fake(parent_node: Node) -> bool:
+	if !FEAGI_JS.is_web_build():
+		return false # Not possible on nonweb builds
+	_FEAGI_interface = FEAGI_RunTime_FEAGIInterface.new()
+	_FEAGI_interface.interface_closed.connect(_on_interface_close)
+	_FEAGI_interface.name = "FEAGI Networking PM"
+	parent_node.add_child(_FEAGI_interface)
 	
+	#NOTE We cannot register debug interface because this was exported
+	
+	return _FEAGI_interface.setup_postmessage()
 
 func send_configurator_and_enable(initial_configurator_json: StringName) -> void:
 	# check network active
 	if !_FEAGI_interface:
 		return
 	if !_FEAGI_interface.connection_active:
+		return
+	if _FEAGI_interface.mode == _FEAGI_interface.MODE.POSTMESSAGE:
+		push_warning("FEAGI: No need to send configurator in PostMessage mode!")
 		return
 	
 	# check URl params, update configurator
@@ -96,12 +112,6 @@ func send_configurator_and_enable(initial_configurator_json: StringName) -> void
 	_FEAGI_interface.send_final_configurator_JSON(initial_configurator_json)
 	print("FEAGI: Sent the Configurator JSON!")
 	
-	# connect motor signals
-	if _debug_interface:
-		_FEAGI_interface.socket_recieved_motor_data.connect(_debug_interface.alert_debugger_about_motor_update)
-	pass
-
-
 func on_sensor_tick() -> void:
 	# Update all sensor values
 	for sensor_IO in _FEAGI_sensors_reference_arr:
@@ -112,5 +122,5 @@ func on_sensor_tick() -> void:
 	if _FEAGI_interface:
 		_FEAGI_interface.on_sensor_tick()
 
-func _on_socket_close() -> void:
+func _on_interface_close() -> void:
 	_FEAGI_interface = null
