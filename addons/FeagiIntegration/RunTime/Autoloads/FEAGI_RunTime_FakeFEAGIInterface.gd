5f44436dc824a9f38717bee558779e55dda25e67
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-10-30 14:56:33 -0400

    ASDGEVLIJH:BUgasdr;ouEIJHG;RWAOHEJSIU

--- addons/FeagiIntegration/RunTime/Autoloads/FEAGI_RunTime_FakeFEAGIInterface.gd
+++ addons/FeagiIntegration/RunTime/Autoloads/FEAGI_RunTime_FakeFEAGIInterface.gd
@@ -13,10 +13,12 @@ func _init() -> void:
 func _on_recieve_message(incoming_JS_data) -> void:
 	print("1: " + str(incoming_JS_data))
 	print("2: " + (incoming_JS_data as String))
+	print("42: + " + incoming_JS_data[0]) # WHY  https://docs.godotengine.org/cs/4.x/tutorials/platform/web/javascript_bridge.html#callbacks
 	if incoming_JS_data.data:
 		print("3: " + (incoming_JS_data.data))
 	if incoming_JS_data.message:
 		print("4: " + (incoming_JS_data.message))
 	print("99: " + (incoming_JS_data)["data"])
 	print("990: " + (incoming_JS_data as Dictionary)["data"])
+	
 	recieved_bytes.emit(incoming_JS_data.data as PackedByteArray)
