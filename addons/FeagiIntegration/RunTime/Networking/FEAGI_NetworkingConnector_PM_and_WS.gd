ebe0ce8941bfcd479717a7e36a9edfb3d03e450a
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-11-06 17:19:39 -0500

    disable motor input from ws

--- addons/FeagiIntegration/RunTime/Networking/FEAGI_NetworkingConnector_PM_and_WS.gd
+++ addons/FeagiIntegration/RunTime/Networking/FEAGI_NetworkingConnector_PM_and_WS.gd
@@ -20,7 +20,7 @@ func setup_post_message_and_websocket(full_connector_WS_address: StringName) -> 
 		_WS = null
 		return false
 	_PM.recieved_bytes.connect(func(data: PackedByteArray): recieved_bytes.emit(data)) # proxy data
-	_WS.recieved_bytes.connect(func(data: PackedByteArray): recieved_bytes.emit(data))
+	#_WS.recieved_bytes.connect(func(data: PackedByteArray): recieved_bytes.emit(data)) # NOTE we dont want WS motor in
 	_PM.connection_closed.connect(func(): connection_closed.emit()) # lol
 	_WS.connection_closed.connect(func(): connection_closed.emit())
 	_connection_active = true
