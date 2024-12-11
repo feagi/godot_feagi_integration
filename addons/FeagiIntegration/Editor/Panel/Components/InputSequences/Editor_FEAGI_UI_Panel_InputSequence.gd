fd317204e70dc51a469e507cfdc06e9e87e6766e
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-12-11 14:14:23 -0500

    more UI work

--- addons/FeagiIntegration/Editor/Panel/Components/InputSequences/Editor_FEAGI_UI_Panel_InputSequence.gd
+++ addons/FeagiIntegration/Editor/Panel/Components/InputSequences/Editor_FEAGI_UI_Panel_InputSequence.gd
@@ -1,3 +1,38 @@
 @tool
 extends VBoxContainer
 class_name Editor_FEAGI_UI_Panel_InputSequence
+
+const SEQUENCE_ELEMENT_PREFAB: PackedScene = preload("res://addons/FeagiIntegration/Editor/Panel/Components/InputSequences/Editor_FEAGI_UI_Panel_InputSequenceElement.tscn")
+
+var sequence_name: StringName:
+	get: 
+		if _sequence_name_UI:
+			return _sequence_name_UI.text
+		return "NOT SETUP"
+
+var _sequence_name_UI: Label
+var _steps_holder: VBoxContainer
+
+func setup(given_sequence_name: StringName, sequence: FEAGI_EmuPredefinedInputSequence = null) -> void:
+	_sequence_name_UI = $SequenceName
+	_steps_holder = $MarginContainer/PanelContainer/StepsHolder
+	
+	_sequence_name_UI.text = given_sequence_name
+	
+	if sequence:
+		for step in sequence.sequence:
+			_append_step(step)
+
+func _append_step(step: FEAGI_EmuPredefinedInput = null) -> Editor_FEAGI_UI_Panel_InputSequenceElement:
+	var appending: Editor_FEAGI_UI_Panel_InputSequenceElement = SEQUENCE_ELEMENT_PREFAB.instantiate()
+	appending.setup(step)
+	_steps_holder.add_child(appending)
+	appending.request_add_sequence_element_to_index.connect(_request_adding_new_step_at_index)
+	return appending
+
+func _request_adding_new_step_at_index(index: int) -> void:
+	if index >= _steps_holder.get_child_count():
+		push_error("FEAGI Configurator: Unable to add input sequence step at an invalid index!")
+		return
+	var new_step: Editor_FEAGI_UI_Panel_InputSequenceElement = _append_step()
+	_steps_holder.move_child(new_step, index)
--- addons/FeagiIntegration/Editor/Panel/Components/InputSequences/Editor_FEAGI_UI_Panel_InputSequenceElement.gd
+++ addons/FeagiIntegration/Editor/Panel/Components/InputSequences/Editor_FEAGI_UI_Panel_InputSequenceElement.gd
@@ -8,7 +8,7 @@ signal request_add_sequence_element_to_index(requested_index: int)
 var _step1_selection: VBoxContainer
 var _step2_delay: VBoxContainer
 var _step2_input: VBoxContainer
-var _add_before: Button
+var _option_buttons: HBoxContainer
 var _emu_input_config: Editor_FEAGI_UI_Panel_EmuInputConfiguration
 var _delay_length_UI: SpinBox
 var _input_hold_length_UI: SpinBox
@@ -18,7 +18,7 @@ func setup(predefined_input: FEAGI_EmuPredefinedInput = null) -> void:
 	_step1_selection = $PanelContainer/MarginContainer/VBoxContainer/Step1_type
 	_step2_delay = $PanelContainer/MarginContainer/VBoxContainer/Step2_Delay
 	_step2_input = $PanelContainer/MarginContainer/VBoxContainer/Step2_Input
-	_add_before = $PanelContainer/MarginContainer/VBoxContainer/AddBefore
+	_option_buttons = $PanelContainer/MarginContainer/VBoxContainer/options
 	_emu_input_config = $PanelContainer/MarginContainer/VBoxContainer/Step2_Input/EditorFeagiUiPanelEmuInputConfiguration
 	_delay_length_UI = $PanelContainer/MarginContainer/VBoxContainer/Step2_Delay/HBoxContainer/SpinBox
 	_input_hold_length_UI = $PanelContainer/MarginContainer/VBoxContainer/Step2_Input/HBoxContainer/SpinBox
@@ -53,13 +53,13 @@ func _step1_choose_mode() -> void:
 	_step1_selection.visible = true
 	_step2_delay.visible = false
 	_step2_input.visible = false
-	_add_before.visible = false
+	_option_buttons.visible = false
 
 func _step2_choose_delay(delay: float = 0.1) -> void:
 	_step1_selection.visible = false
 	_step2_delay.visible = true
 	_step2_input.visible = false
-	_add_before.visible = true
+	_option_buttons.visible = true
 	
 	_delay_length_UI.value = delay
 
@@ -67,6 +67,9 @@ func _step2_choose_input(input: FEAGI_EmuInput_Abstract = null, seconds_to_hold:
 	_step1_selection.visible = false
 	_step2_delay.visible = false
 	_step2_input.visible = true
-	_add_before.visible = true
+	_option_buttons.visible = true
 	_emu_input_config.setup("Predefined Input", FEAGI_IOConnector_Motor_Base.INPUT_EMULATOR_DATA_TYPE.FLOAT_0_TO_1, input)
 	_input_hold_length_UI.value = seconds_to_hold
+
+func _add_step_before_pressed():
+	request_add_sequence_element_to_index.emit(get_index())
