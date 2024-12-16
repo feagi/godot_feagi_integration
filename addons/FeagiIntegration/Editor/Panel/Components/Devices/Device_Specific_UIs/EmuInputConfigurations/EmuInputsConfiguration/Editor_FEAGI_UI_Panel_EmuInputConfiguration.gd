eaff23b7e04d36636f5d9ebd99d4bfbf88d6207c
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-12-16 14:34:02 -0500

    more theming stuff

--- addons/FeagiIntegration/Editor/Panel/Components/Devices/Device_Specific_UIs/EmuInputConfigurations/EmuInputsConfiguration/Editor_FEAGI_UI_Panel_EmuInputConfiguration.gd
+++ addons/FeagiIntegration/Editor/Panel/Components/Devices/Device_Specific_UIs/EmuInputConfigurations/EmuInputsConfiguration/Editor_FEAGI_UI_Panel_EmuInputConfiguration.gd
@@ -1,5 +1,5 @@
 @tool
-extends VBoxContainer
+extends MarginContainer
 class_name Editor_FEAGI_UI_Panel_EmuInputConfiguration
 
 var _data_type: FEAGI_IOConnector_Motor_Base.INPUT_EMULATOR_DATA_TYPE
@@ -13,22 +13,22 @@ var _3keyboard: BoxContainer
 var _reset: Button
 
 func setup(motor_output_name: StringName, motor_output_datatype: FEAGI_IOConnector_Motor_Base.INPUT_EMULATOR_DATA_TYPE, preinit_emuInput: FEAGI_EmuInput_Abstract = null) -> void:
-	$GodotInputName.text = motor_output_name
+	$PanelContainer/HBoxContainer/GodotInputName.text = "[center][b][u]%s[/u][/b][/center]" % motor_output_name
 	_data_type = motor_output_datatype
 	match(motor_output_datatype):
 		FEAGI_IOConnector_Motor_Base.INPUT_EMULATOR_DATA_TYPE.FLOAT_0_TO_1:
-			$GodotInputType.text = "0 to 1"
+			$PanelContainer/HBoxContainer/GodotInputType.text = "0 to 1"
 		FEAGI_IOConnector_Motor_Base.INPUT_EMULATOR_DATA_TYPE.FLOAT_M1_TO_1:
-			$GodotInputType.text = "-1 to 1"
+			$PanelContainer/HBoxContainer/GodotInputType.text = "-1 to 1"
 		FEAGI_IOConnector_Motor_Base.INPUT_EMULATOR_DATA_TYPE.VEC2:
-			$GodotInputType.text = "<-1, -1> to <1, 1>"
-	_1button = $VBoxContainer/start
-	_2scroll = $VBoxContainer/emuselect
-	_2scroll_godotInputEvent = $VBoxContainer/emuselect/VBoxContainer/GodotInputEvent
-	_2scroll_keyboard = $VBoxContainer/emuselect/VBoxContainer/Keyboard
-	_3godotInputEvent = $VBoxContainer/InputEventConfig
-	_3keyboard = $VBoxContainer/Keyboard
-	_reset = $VBoxContainer/Reset
+			$PanelContainer/HBoxContainer/GodotInputType.text = "<-1, -1> to <1, 1>"
+	_1button = $PanelContainer/HBoxContainer/VBoxContainer/start
+	_2scroll = $PanelContainer/HBoxContainer/VBoxContainer/emuselect
+	_2scroll_godotInputEvent = $PanelContainer/HBoxContainer/VBoxContainer/emuselect/VBoxContainer/GodotInputEvent
+	_2scroll_keyboard = $PanelContainer/HBoxContainer/VBoxContainer/emuselect/VBoxContainer/Keyboard
+	_3godotInputEvent = $PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig
+	_3keyboard = $PanelContainer/HBoxContainer/VBoxContainer/Keyboard
+	_reset = $PanelContainer/HBoxContainer/VBoxContainer/Reset
 	
 	if !preinit_emuInput:
 		_step_1_set_to_unconfigured()
@@ -42,26 +42,26 @@ func setup(motor_output_name: StringName, motor_output_datatype: FEAGI_IOConnect
 func export_emu_input() -> FEAGI_EmuInput_Abstract:
 	if _3godotInputEvent.visible:
 		var output: FEAGI_EmuInput_GodotInputEvent = FEAGI_EmuInput_GodotInputEvent.new()
-		var dropdown: OptionButton = $VBoxContainer/InputEventConfig/HBoxContainer/OptionButton
+		var dropdown: OptionButton = $PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer/OptionButton
 		output.godot_input_event_name = dropdown.get_item_text(dropdown.selected)
-		output.use_bang_bang_instead_of_actual_value = $VBoxContainer/InputEventConfig/HBoxContainer2/enablethresholding.button_pressed
-		output.bang_bang_threshold = $VBoxContainer/InputEventConfig/HBoxContainer3/SpinBox.value
+		output.use_bang_bang_instead_of_actual_value = $PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer2/enablethresholding.button_pressed
+		output.bang_bang_threshold = $PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer3/SpinBox.value
 		return output
 	
 	if _3keyboard.visible:
 		var output: FEAGI_EmuInput_KeyboardPress = FEAGI_EmuInput_KeyboardPress.new()
-		var dropdown: OptionButton = $VBoxContainer/Keyboard/HBoxContainer/OptionButton
+		var dropdown: OptionButton = $PanelContainer/HBoxContainer/VBoxContainer/Keyboard/HBoxContainer/OptionButton
 		output.key_to_press = dropdown.get_item_id(dropdown.selected)
-		output.bang_bang_threshold = $VBoxContainer/Keyboard/HBoxContainer3/SpinBox.value
+		output.bang_bang_threshold = $PanelContainer/HBoxContainer/VBoxContainer/Keyboard/HBoxContainer3/SpinBox.value
 		return output
 	
 	return null
 
 ## Used in the case of sequences, when we dont care to set FEAGI thresholding since we ourselves are supplying the information, so no need to show those options
 func hide_feagi_threshold_UI() -> void:
-	$VBoxContainer/InputEventConfig/HBoxContainer2.visible = false
-	$VBoxContainer/InputEventConfig/HBoxContainer3.visible = false
-	$VBoxContainer/Keyboard/HBoxContainer3.visible = false
+	$PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer2.visible = false
+	$PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer3.visible = false
+	$PanelContainer/HBoxContainer/VBoxContainer/Keyboard/HBoxContainer3.visible = false
 
 func _step_1_set_to_unconfigured() -> void:
 	_1button.visible = true
@@ -88,7 +88,7 @@ func _step_3_configure_godotInputEvent(preset_godot_event: FEAGI_EmuInput_GodotI
 	_3keyboard.visible = false
 	_reset.visible = true
 	
-	var dropdown: OptionButton = $VBoxContainer/InputEventConfig/HBoxContainer/OptionButton
+	var dropdown: OptionButton = $PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer/OptionButton
 	InputMap.load_from_project_settings() # ensure we have the full info!
 	var possible_actions: Array[StringName] = InputMap.get_actions()
 	dropdown.clear()
@@ -106,8 +106,8 @@ func _step_3_configure_godotInputEvent(preset_godot_event: FEAGI_EmuInput_GodotI
 			dropdown.selected = 1 + action_index
 		else:
 			push_error("FEAGI Configurator: Unable to find preset input event %s in this project's input event settings!" % preset_godot_event.godot_input_event_name)
-	$VBoxContainer/InputEventConfig/HBoxContainer2/enablethresholding.button_pressed = preset_godot_event.use_bang_bang_instead_of_actual_value
-	$VBoxContainer/InputEventConfig/HBoxContainer3/SpinBox.value = preset_godot_event.bang_bang_threshold
+	$PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer2/enablethresholding.button_pressed = preset_godot_event.use_bang_bang_instead_of_actual_value
+	$PanelContainer/HBoxContainer/VBoxContainer/InputEventConfig/HBoxContainer3/SpinBox.value = preset_godot_event.bang_bang_threshold
 	
 
 func _step_3_configure_keyboard(preset_keyboard_event: FEAGI_EmuInput_KeyboardPress = null) -> void:
@@ -117,7 +117,7 @@ func _step_3_configure_keyboard(preset_keyboard_event: FEAGI_EmuInput_KeyboardPr
 	_3keyboard.visible = true
 	_reset.visible = true
 	
-	var dropdown: OptionButton = $VBoxContainer/Keyboard/HBoxContainer/OptionButton
+	var dropdown: OptionButton = $PanelContainer/HBoxContainer/VBoxContainer/Keyboard/HBoxContainer/OptionButton
 	dropdown.clear()
 	# We have a none key already with an int value of 0 (both for possible keys and Key)
 	for possible in FEAGI_EmuInput_KeyboardPress.SUPPORTED_KEY.keys():
@@ -132,4 +132,4 @@ func _step_3_configure_keyboard(preset_keyboard_event: FEAGI_EmuInput_KeyboardPr
 		dropdown.select(index_to_select)
 	else:
 		push_error("FEAGI Configurator: Unsupported key defined for keyboard input! Defaulting to None!")
-	$VBoxContainer/Keyboard/HBoxContainer3/SpinBox.value = preset_keyboard_event.bang_bang_threshold
+	$PanelContainer/HBoxContainer/VBoxContainer/Keyboard/HBoxContainer3/SpinBox.value = preset_keyboard_event.bang_bang_threshold
