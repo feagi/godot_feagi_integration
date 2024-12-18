740ebb4abbff5a99e8eaf45e2823665bb7c4598c
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-12-18 17:28:35 -0500

    fix UI not loading mouse UI

--- addons/FeagiIntegration/Editor/Panel/Components/Devices/Device_Specific_UIs/EmuInputConfigurations/EmuInputsConfiguration/Editor_FEAGI_UI_Panel_EmuInputConfiguration.gd
+++ addons/FeagiIntegration/Editor/Panel/Components/Devices/Device_Specific_UIs/EmuInputConfigurations/EmuInputsConfiguration/Editor_FEAGI_UI_Panel_EmuInputConfiguration.gd
@@ -52,6 +52,10 @@ func setup(motor_output_name: StringName, motor_output_datatype: FEAGI_IOConnect
 		_step_3_configure_godotInputEvent(preinit_emuInput)
 	if preinit_emuInput is FEAGI_EmuInput_KeyboardPress:
 		_step_3_configure_keyboard(preinit_emuInput)
+	if preinit_emuInput is FEAGI_EmuInput_MouseClick:
+		_step_3_configure_mouseClick(preinit_emuInput)
+	if preinit_emuInput is FEAGI_EmuInput_MouseMotion:
+		_step_3_configure_mouseMotion(preinit_emuInput)
 
 ## Exports an emu input object. Will either export a specific child of [FEAGI_EmuInput_Abstract] or null if none is selected (or if something is wrong)
 func export_emu_input() -> FEAGI_EmuInput_Abstract:
